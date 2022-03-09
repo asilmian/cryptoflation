8a8ef6cc00fc039cf0e3e7efe0971b64c01517e5
Asil Mian <asilmian@gmail.com>
2022-03-10 00:54:38 +1100

    working inflation calc with hard coded initial investment

--- main.py
+++ main.py
@@ -3,10 +3,12 @@
 import fire
 import requests
 import time
+from cryptoflation.inflation_calculator import calc_pos, calc_pos_w_inflation
 from secret import annual_inflation_rate
 from coinspot import get_coin_spot_data
+import inflation_calculator
 
-def total_profit_w_inflation(rate : float = annual_inflation_rate) -> float:
+def total_profit_w_inflation(rate : float = annual_inflation_rate):
     """
     Returns position of crypto holdings adjusted for previous years' inflation
     Assumes inflation is linear across the year 
@@ -14,21 +16,13 @@ def total_profit_w_inflation(rate : float = annual_inflation_rate) -> float:
     :return: position of cryptocurrency holding in coinspot adjusted for inflation
     """
 
+    coin_data = get_coin_spot_data()
+    curr_pos = calc_pos(coin_data['total_balance'])
+    print("Your current position is {:+.2f}".format(curr_pos))
+    curr_pos = calc_pos_w_inflation(coin_data['total_balance'])
+    print("Your current position with inflation is {:+.2f}".format(curr_pos))
 
-    get_coin_spot_data()
-
-    return 1.0
-    # curr_value_AUD = calc_current_crypto_value(coin_spot_data)
-
-    # total_AUD_invested = calc_total_AUD(coin_spot_data)
-
-    # curr_pos = curr_value_AUD - total_AUD_invested
-
-    # est_inflation = estmiate_inflation()
-
-    # inflated_val = total_AUD_invested * (1+ est_inflation)
-
-    # curr_pos_w_inflation = curr_value_AUD - inflated_val
+    return
 
 
 
@@ -37,5 +31,5 @@ def total_profit_w_inflation(rate : float = annual_inflation_rate) -> float:
 
 if __name__=="__main__":
     fire.Fire({
-        "test" : total_profit_w_inflation
+        "calc_position" : total_profit_w_inflation
     })
